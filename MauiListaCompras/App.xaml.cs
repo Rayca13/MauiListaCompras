7f6d3f511725226cd63a9586f9d681f2e9faaf93
Usuario <Usuario@CPD02-M19>
2024-03-04 12:20:40 -0300

    avanço no projeto

--- MauiListaCompras/App.xaml.cs
+++ MauiListaCompras/App.xaml.cs
@@ -1,8 +1,30 @@
-﻿namespace MauiListaCompras
+﻿using MauiListaCompras.Helpers;
+
+namespace MauiListaCompras
 {
     public partial class App : Application
     {
-        public App()
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null) 
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                             "banco_sql_compras.db3"
+                        );
+
+                    _db = new SQLiteDatabaseHelper( path );
+                }//fechar if verificando se _db é null
+
+                return _db;
+            }//fechar método get
+        }
+        public App() 
         {
             InitializeComponent();
 
--- MauiListaCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiListaCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MauiListaCompras.Models;
+﻿using MauiListaCompras.Models;
 using SQLite;
 
 namespace MauiListaCompras.Helpers
@@ -21,5 +16,30 @@ namespace MauiListaCompras.Helpers
         {
             return _conn.InsertAsync(p);
         }
-    }
-}
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDADE Produto SET descricao=?, " + "Quantidade =?, Preco=? WHERE Id=?";
+            return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+        }
+
+        public Task<List<Produto>> GetAll() 
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<int> Delete(int id) 
+        {
+            return _conn.Table<Produto> ().DeleteAsync(
+                i => i.Id == id);
+        }
+
+        public Task<List<Produto>> Search(string q) 
+        {
+            string sql = $"SELECT * FROM Produto WHERE" + "Descricao LIKE '% " + q + "%'";
+
+            return _conn.QueryAsync<Produto> (sql);
+        }
+
+    }// fecha classe
+}//fecha nomespace
